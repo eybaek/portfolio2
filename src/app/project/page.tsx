4c8254202284b4eb84230f40b3567a40ea93eadf
eunyoung.baek <eunyoung.baek@yanolja.com>
2023-10-25 12:47:55 +0900

    project 설명 추가
    project 설명 추가

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -10,9 +10,10 @@ function Project() {
               <img src="https://firebasestorage.googleapis.com/v0/b/eybaek-44875.appspot.com/o/%E1%84%89%E1%85%A5%E1%86%AB%E1%84%87%E1%85%AE%E1%86%AF1.png?alt=media&token=4be1488a-74c0-46b7-84c2-b970907f7f79&_gl=1*971ybl*_ga*MjAyNDMxMzAxMC4xNjk3NjM2OTU4*_ga_CW55HF8NVT*MTY5ODA2NzI0Ni4yLjEuMTY5ODA2ODMyMy41OS4wLjA." alt="" />
             </figure>
             <div className="article-body">
-              <h2>Web Service</h2>
+              <h2>YANOLJA</h2>
+              <h2>Web service</h2>
               <p>
-                Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                선불전자지급수단, 숙박대전, 놀이대전 개발 
               </p>
               <a href="#" className="read-more">
                 Read more <span className="sr-only">about this is some title</span>
@@ -29,9 +30,9 @@ function Project() {
               <img src="https://firebasestorage.googleapis.com/v0/b/eybaek-44875.appspot.com/o/clay1.JPG?alt=media&token=2e5fb22e-b784-4911-abb6-76ead9ecf1da&_gl=1*zgy0jz*_ga*MjAyNDMxMzAxMC4xNjk3NjM2OTU4*_ga_CW55HF8NVT*MTY5ODA2NzI0Ni4yLjEuMTY5ODA2ODQ0Ni4xLjAuMA.." alt="" />
             </figure>
             <div className="article-body">
-              <h2>Web Application</h2>
+              <h2>Humax Networks Web & Flutter App</h2>
               <p>
-                Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                Web Client UI 개발
               </p>
               <a href="#" className="read-more">
                 Read more <span className="sr-only">about this is some title</span>
@@ -48,9 +49,9 @@ function Project() {
               <img src="https://firebasestorage.googleapis.com/v0/b/eybaek-44875.appspot.com/o/rego1.png?alt=media&token=fdecd0e7-2b09-475f-a92e-9747ffec2368&_gl=1*1mx0q6g*_ga*MjAyNDMxMzAxMC4xNjk3NjM2OTU4*_ga_CW55HF8NVT*MTY5ODA2NzI0Ni4yLjEuMTY5ODA2ODM4Ny42MC4wLjA." alt="" />
             </figure>
             <div className="article-body">
-              <h2>Web Application</h2>
+              <h2>Humax Web Application</h2>
               <p>
-                Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                Web Client UI 개발
               </p>
               <a href="#" className="read-more">
                 Read more <span className="sr-only">about this is some title</span>
@@ -67,9 +68,9 @@ function Project() {
               <img src="https://firebasestorage.googleapis.com/v0/b/eybaek-44875.appspot.com/o/hgs2.png?alt=media&token=fd0d7e4f-f11f-4e78-8a72-b7aa3cc8931b&_gl=1*9qyci7*_ga*MjAyNDMxMzAxMC4xNjk3NjM2OTU4*_ga_CW55HF8NVT*MTY5ODA2NzI0Ni4yLjEuMTY5ODA2ODUwMS43LjAuMA.." alt="" />
             </figure>
             <div className="article-body">
-              <h2>Web Application</h2>
+              <h2>Humax Settop box Web Application</h2>
               <p>
-                Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                Settop box Web UI 개발 및 서비스 기획
               </p>
               <a href="#" className="read-more">
                 Read more <span className="sr-only">about this is some title</span>
@@ -86,9 +87,9 @@ function Project() {
               <img src="https://firebasestorage.googleapis.com/v0/b/eybaek-44875.appspot.com/o/mobile3.png?alt=media&token=e7dad6ac-6957-4f68-954a-d2f4ceccd702&_gl=1*1p8bgiv*_ga*MjAyNDMxMzAxMC4xNjk3NjM2OTU4*_ga_CW55HF8NVT*MTY5ODA2NzI0Ni4yLjEuMTY5ODA2ODU3Ni41MC4wLjA." alt="" />
             </figure>
             <div className="article-body">
-              <h2>Android Application</h2>
+              <h2>HUMAX Android Application</h2>
               <p>
-                Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                Android Application 개발 및 REST API 설계
               </p>
               <a href="#" className="read-more">
                 Read more <span className="sr-only">about this is some title</span>
@@ -105,9 +106,9 @@ function Project() {
               <img src="https://firebasestorage.googleapis.com/v0/b/eybaek-44875.appspot.com/o/%E1%84%89%E1%85%B3%E1%84%86%E1%85%A1%E1%84%90%E1%85%B3%E1%84%91%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A6%E1%84%8B%E1%85%B5.png?alt=media&token=1bbaf384-e4ed-4688-8a27-bcfaf5422262&_gl=1*1kbm759*_ga*MjAyNDMxMzAxMC4xNjk3NjM2OTU4*_ga_CW55HF8NVT*MTY5ODA2NzI0Ni4yLjEuMTY5ODA2ODU5OS4yNy4wLjA." alt="" />
             </figure>
             <div className="article-body">
-              <h2>Android Application</h2>
+              <h2>Valups Android Application</h2>
               <p>
-                Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                Android Application 개발 및 DLNA 라이브러리 개발
               </p>
               <a href="#" className="read-more">
                 Read more <span className="sr-only">about this is some title</span>
